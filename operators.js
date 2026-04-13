6ab02157e658a39f4f8cb135b7b53477f40108ae
Tony Kainga <tonykainga@gmail.com>
2026-04-13 17:09:53 +0300

    files

--- operators.js
+++ operators.js
@@ -11,13 +11,11 @@ console.log(a+b);
 console.log(a-b);
 // // a*b*c
  console.log(a*b*c);
-// // %: modulus to get the remainder after division
+// // %: modulus to get the remainder after division 
 console.log(c%b);
 console.log(c%d);
 // // **:exponentiation 
 console.log(b**d); // raising to the power
-
-
 // // Assignment operators help store and update values (=)
  a=25;
  a+=3;
@@ -25,14 +23,11 @@ console.log(b**d); // raising to the power
  a-=4;
  a*=3;
 console.log(a);
-
 // // Comparison operators help compare values
-
 console.log(a>b);
 console.log(d<a);
 console.log(b>=c);
 console.log(a<=c);
-
 // // Equality Operators: ==(loose Equality) and ===(strict equality)
 console.log(6=="6");
 console.log(6==="6");
@@ -41,9 +36,7 @@ console.log(10!=5);//true
 console.log(10!=="10");//true
 console.log(10!==10);//false
 console.log("Hello"==="World");//false
-
 // Logical operators help combine conditions. (&&,||,!)
-
 console.log(true&&true);
 console.log(true&&false);
 console.log(true||false);
