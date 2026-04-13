ed12620d180d8a238550cf6845780dcce92a4154
Tony Kainga <tonykainga@gmail.com>
2026-04-13 17:04:39 +0300

    files

--- operators.js
+++ operators.js
@@ -1,46 +1,53 @@
-// operators are symbols that tell JavaScript what actions to perform 
+// // operators are symbols that tell JavaScript what actions to perform 
 
-// Arithmetic operators help with calclulations( +,-,*,/,%)
+// // Arithmetic operators help with calclulations( +,-,*,/,%)
 let a=10;
-let b=5
-let c=20
+let b=5;
+let c=20;
 let d=3
-// a+b
+// // a+b
 console.log(a+b);
-// a-b
+// // a-b
 console.log(a-b);
-// a*b*c
-console.log(a*b*c);
-// %: modulus to get the remainder after division
+// // a*b*c
+ console.log(a*b*c);
+// // %: modulus to get the remainder after division
 console.log(c%b);
 console.log(c%d);
-// **:exponentiation 
+// // **:exponentiation 
 console.log(b**d); // raising to the power
 
 
-// Assignment operators help store and update values (=)
-a=25;
-a+=3;//a+3=a
-a-=4;
-a*=3;
+// // Assignment operators help store and update values (=)
+ a=25;
+ a+=3;
+ a=a+3;
+ a-=4;
+ a*=3;
 console.log(a);
 
-// Comparison operators help compare values
+// // Comparison operators help compare values
 
 console.log(a>b);
 console.log(d<a);
 console.log(b>=c);
 console.log(a<=c);
 
-// Equality Operators: ==(loose Equality) and ===(strict equality)
+// // Equality Operators: ==(loose Equality) and ===(strict equality)
 console.log(6=="6");
 console.log(6==="6");
-// loose inequality(!=) & strict inequality (!==)
-console.log(10!=5)//true
-console.log(10!=="10")//true
+// // loose inequality(!=) & strict inequality (!==)
+console.log(10!=5);//true
+console.log(10!=="10");//true
 console.log(10!==10);//false
-console.log("Hello"==="World")//false
+console.log("Hello"==="World");//false
 
+// Logical operators help combine conditions. (&&,||,!)
 
-// Logical operators help combine conditions.
+console.log(true&&true);
+console.log(true&&false);
+console.log(true||false);
+console.log(false||false);
+console.log(!false);
+console.log(!true);
 
