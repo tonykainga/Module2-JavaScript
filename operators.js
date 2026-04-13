7fa60a4ad32517f0165b849695f546e212b8c13c
Tony Kainga <tonykainga@gmail.com>
2026-04-13 15:21:41 +0300

    files

---
+++ operators.js
@@ -0,0 +1,14 @@
+// operators are symbols that tell JavaScript what actions to perform 
+// Arithmetic operators help with calclulations( +,-,*,/,%)
+let a=10;
+let b=5
+let c=20
+// a+b
+console.log(a+b);
+// a-b
+console.log(a-b);
+// a*b*c
+console.log(a*b*c);
+// Assignment operators help store and update values
+// Comparison operators help compare values
+// Logical operators help combine conditions.
