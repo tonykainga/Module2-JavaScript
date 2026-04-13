0b51eea45f50882462a285e8f747b9b7ad0aa22e
Tony Kainga <tonykainga@gmail.com>
2026-04-13 19:16:17 +0300

    files

--- test-file.js
+++ test-file.js
@@ -1,38 +1,46 @@
-let a = 10;
-let b = 5;
-let c = 20;
-let d = 3;
+// operators are symbols that tell JavaScript what actions to perform 
 
-console.log("a+b =", a + b);
-console.log("a-b =", a - b);
-console.log("a*b*c =", a * b * c);
-console.log("c%b =", c % b);
-console.log("c%d =", c % d);
-console.log("b**d =", b ** d);
-
-a = 25;
-a += 3;
-a = a + 3;
-a -= 4;
-a *= 3;
-console.log("a =", a);
-
-console.log("a>b =", a > b);
-console.log("d<a =", d < a);
-console.log("b>=c =", b >= c);
-console.log("a<=c =", a <= c);
-
-console.log('6=="6" =', 6 == "6");
-console.log('6==="6" =', 6 === "6");
-
-console.log("10!=5 =", 10 != 5);
-console.log('10!=="10" =', 10 !== "10");
-console.log("10!==10 =", 10 !== 10);
-console.log('"Hello"==="World" =', "Hello" === "World");
-
-console.log("true&&true =", true && true);
-console.log("true&&false =", true && false);
-console.log("true||false =", true || false);
-console.log("false||false =", false || false);
-console.log("!false =", !false);
-console.log("!true =", !true);
+// Arithmetic operators help with calclulations( +,-,*,/,%)
+let a=10;
+let b=5;
+let c=20;
+let d=3;
+// // a+b
+console.log(a+b);
+// // a-b
+console.log(a-b);
+// *b*c
+ console.log(a*b*c);
+// % modulus to get the remainder after division 
+console.log(c % b);
+console.log(c % d);
+// **:exponentiation 
+//b**d)
+console.log(b**3);
+// Assignment operators help store and update values (=)
+ a=25;
+ a+=3;
+ a=a+3;
+ a-=4;
+ a*=3;
+console.log(a);
+// Comparison operators help compare values
+console.log(a>b);
+console.log(d<a);console.log(b**5);
+console.log(b>=c);
+console.log(a<=c);
+// Equality Operators: ==(loose Equality) and ===(strict equality)
+console.log(6=="6");
+console.log(6==="6");
+// loosequality(!=) & strict inequality (!==)
+console.log(10!=5);//true
+console.log(10!=="10");//true
+console.log(10!==10);//false
+console.log("Hello"==="World");//false
+// Logical operators help combine conditions. (&&,||,!)
+console.log(true&&true);
+console.log(true&&false);
+console.log(true||false);
+console.log(false||false);
+console.log(!false);
+console.log(!true);
