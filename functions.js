cec91ec39a994651b441a324bb33451a6fa61ed9
Tony Kainga <tonykainga@gmail.com>
2026-04-21 13:11:45 +0300

    files

--- functions.js
+++ functions.js
@@ -1,1 +1,18 @@
 // what is a function in JS
+// console.log("Welcome to class")
+// console.log("welcometo class")
+// console.log("welcome to class")
+//Syntax
+
+// function FunctionName(){
+//     block of code
+// }
+// Definfing a function 
+ function greet(){
+    console.log("Hello");
+}
+
+// CALLING A FUNCTION
+greet();
+greet();
+greet();
