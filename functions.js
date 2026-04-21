aeeca2d80765fe8daa2d49edb72783431308bf6a
Tony Kainga <tonykainga@gmail.com>
2026-04-21 21:55:22 +0300

    files

--- functions.js
+++ functions.js
@@ -17,35 +17,64 @@
 // greet();
 // greet();
 
-function introduce(){
-console.log("hello");
-console.log("My name is Tony");
-console.log("I am learning Javascript")
-}
-introduce();
+// function introduce(){
+// console.log("hello");
+// console.log("My name is Tony");
+// console.log("I am learning Javascript")
+// }
+// introduce();
 
-// // adding parameters in a fucntion 
+// // // adding parameters in a fucntion 
 
-function greet (name){
-    console.log("hello " + name)
-}
-greet("Tony");
-greet("Sarabi");
+// function greet (name){
+//     console.log("hello " + name)
+// }
+// greet("Tony");
+// greet("Sarabi");
 
-function introduce(name,age){
-    console.log("My name is "+name);
-    console.log(`I am ${age} years old`)
-}
-introduce("Tony" ,27);
-introduce("Rose" ,23);
+// function introduce(name,age){
+//     console.log("My name is "+name);
+//     console.log(`I am ${age} years old`)
+// }
+// introduce("Tony" ,27);
+// introduce("Rose" ,23);
 
-function sum(a,b,c,d,e){
-    console.log(a+b+c+d+e);
+// function sum(a,b,c,d,e){
+//     console.log(a+b+c+d+e);
+// }
+// sum(2,7,34,90,56);
+// function showName(){
+//     // console.log("Tony")
+//     return "Tony"
+// }
+// let student=showName();
+// console.log(student);
+
+function sumOfTwoNumbers(x,y){
+    return x+y;
 }
-sum(2,7,34,90,56);
-function showName(){
-    // console.log("Tony")
-    return "Tony"
+sumOfTwoNumbers(100,89);
+console.log(sumOfTwoNumbers(100,89));
+
+let sum=sumOfTwoNumbers(100,89)//189
+sum+=90;//sum=sum+90
+console.log(sum);
+
+// Nested Functions
+
+function welcomestudent(){
+    function sayhello(){
+        console.log("Hello Student")
+    };
+    sayhello();
+    console.log("We are learning Javascript Functions")
 }
-let student=showName();
-console.log(student);
+welcomestudent();
+
+function outer(){
+    function greet(name){
+        return `hello my name is ${name}`;
+    }
+         return   greet("John")
+}
+console.log (outer());
