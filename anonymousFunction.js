652155bff19fb8dc7d9824c1c6d599eecae01e37
Tony Kainga <tonykainga@gmail.com>
2026-04-22 16:18:46 +0300

    files

--- anonymousFunction.js
+++ anonymousFunction.js
@@ -1,3 +1,11 @@
-let greet=function(){
-    console.log("Hello");
+let greet=function(name) {
+    console.log("Hello " + name);
 }
+greet("Tony");
+
+let getName= function(){
+     return "Alice";
+}
+ let getStudent=getName();
+ console.log(getStudent) ; 
+
---
+++ arrowsFunctions.js
@@ -0,0 +1,6 @@
+// =>
+// let getName= function(){
+//      return "Alice";
+// }
+let getName=() =>console.log("Alice");
+
