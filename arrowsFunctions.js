eee781f3a6165670a83d5f554acbb66c4c0550b9
Tony Kainga <tonykainga@gmail.com>
2026-04-22 17:00:21 +0300

    files

--- arrowsFunctions.js
+++ arrowsFunctions.js
@@ -3,4 +3,15 @@
 //      return "Alice";
 // }
 let getName=() =>console.log("Alice");
+getName();
 
+let introduce=(name,country) =>{
+    console.log("My name is "+ name);
+    console.log("I come from " +country);
+}
+introduce("Nancy" , "Rwanda");
+
+// let getName=() =>console.log("Alice");
+
+let getName2=()=>{return "Cindy"};
+console.log(getName2());
