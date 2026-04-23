5776b9407d2e9df44f2fe354014acc98ace079bb
Tony Kainga <tonykainga@gmail.com>
2026-04-23 20:38:36 +0300

    files

---
+++ scope.js
@@ -0,0 +1,27 @@
+// scope the area in your code where a variable can be accessed
+
+//  global scope a variable declared outside any block
+
+let school= "banda"
+
+function showSchool(){
+    console.log(school);  
+}
+showSchool()
+console.log(school)
+
+// function scope variable declared within a fucntion
+
+function greet(){
+    let message ="Hello Student";
+    message="welcome"
+    console.log(message);
+}
+greet();
+
+// block scope - only exists withing the block or calibarces
+
+if (true){
+    let color= "blue";
+    console.log(color)
+}
