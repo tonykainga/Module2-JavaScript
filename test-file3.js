d33e3b222dac66a112870ec3a427a38e17aea9bc
Tony Kainga <tonykainga@gmail.com>
2026-05-05 12:09:00 +0300

    files

--- test-file3.js
+++ test-file3.js
@@ -1,4 +1,16 @@
 
-
+let school = "code Academy";// Global variable
+function outerFunction(){
+    let teacher="Nancy";
+    function innerFunction(){
+        let student = "Alice";
+        console.log(school);
+        console.log(teacher);
+        console.log(student)
+    
+    }
+    innerFunction();
+}
+outerFunction();
 
     
