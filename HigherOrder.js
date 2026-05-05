259b7f19b95742eab1abeee33461c5e34e6ed616
Tony Kainga <tonykainga@gmail.com>
2026-05-05 16:15:18 +0300

    files

---
+++ HigherOrder.js
@@ -0,0 +1,18 @@
+// callback and Higher order Functions
+
+function printValue(value){
+    console.log(value)
+}
+// call the function
+printValue(3);
+printValue(true);
+printValue("hello");
+
+function greet (name) {
+    console.log("Hello" + name);
+}
+
+function executeSomething(x){
+    x("Nancy")
+}
+executeSomething(greet);
