c2ba1f0f99e27325423f79b90bc1fdbf1c25f611
Tony Kainga <tonykainga@gmail.com>
2026-05-07 19:24:28 +0300

    files

---
+++ array.js
@@ -0,0 +1,10 @@
+let fruits=['mango','banana','ornage','appla']
+console.log(fruits[1]);
+console.log(fruits[3]);
+// looping; repeating a block of code agin and again until a condtion is no longer true
+// length: the number of items in the array
+console.log(fruits.length)
+// for loop
+for (initialization,condition,update){
+    
+}
